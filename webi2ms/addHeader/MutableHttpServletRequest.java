57ae93d52a75ae356bf13f060201eeac70f9f717
tward392 <twardbiteme1@gmail.com>
2023-07-19 20:09:40 -0400

    Add files via upload

--- webi2ms/addHeader/MutableHttpServletRequest.java
+++ webi2ms/addHeader/MutableHttpServletRequest.java
@@ -1,1 +1,96 @@
+package webi2ms.addHeader;
 
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+//import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+final class MutableHttpServletRequest extends HttpServletRequestWrapper {
+    // holds custom header and value mapping
+    private final Map<String, String> customHeaders;
+
+    public MutableHttpServletRequest(HttpServletRequest request){
+        super(request);
+        this.customHeaders = new HashMap<String, String>();
+    }
+    
+    public void putHeader(String name, String value){
+        this.customHeaders.put(name, value);
+    }
+
+    public String getHeader(String name) {
+        // check the custom headers first
+        String headerValue = customHeaders.get(name);
+        
+        if (headerValue != null){
+            return headerValue;
+        }
+        // else return from into the original wrapped object
+        return ((HttpServletRequest) getRequest()).getHeader(name);
+    }
+
+/****************************************************************************************************************/
+/* TJW didn't fix it
+    public Enumeration<String> getHeaders(String name) {
+        if (null != name && name.equals("userid")) {
+            return new Enumeration<String>() {
+                private boolean hasGetted = false;
+
+                @Override
+                public boolean hasMoreElements() {
+                    return !hasGetted;
+                }
+
+                @Override
+                public String nextElement() {
+                    if (hasGetted) {
+                        throw new NoSuchElementException();
+                    } else {
+                        hasGetted = true;
+                        return customHeaders.get(name);
+                    }
+                }
+            };
+        }
+        return super.getHeaders(name);
+
+    }
+*/
+    @Override
+    public Enumeration<String> getHeaders(String name) {
+        Set<String> set = new HashSet<>();
+        Optional.ofNullable(customHeaders.get(name)).ifPresent(h -> set.add(h));
+        Enumeration<String> e = ((HttpServletRequest) getRequest()).getHeaders(name);
+        while (e.hasMoreElements()) {
+            // add the names of the request headers into the list
+            String n = e.nextElement();
+            set.add(n);
+        }
+        Optional.ofNullable(customHeaders.get(name)).ifPresent(h -> set.add(h));
+        return Collections.enumeration(set);
+    }
+/****************************************************************************************************************/
+    public Enumeration<String> getHeaderNames() {
+        // create a set of the custom header names
+        Set<String> set = new HashSet<String>(customHeaders.keySet());
+        
+        // now add the headers from the wrapped request object
+        @SuppressWarnings("unchecked")
+        Enumeration<String> e = ((HttpServletRequest) getRequest()).getHeaderNames();
+        while (e.hasMoreElements()) {
+            // add the names of the request headers into the list
+            String n = e.nextElement();
+            set.add(n);
+        }
+
+        // create an enumeration from the set and return
+        return Collections.enumeration(set);
+    }
+}
